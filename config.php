86c627e867e34eb04adf6463575bc9aa9a280633
Mostafa Amiri <amiri27@gmail.com>
2014-06-10 20:58:24 +0430

    Update Config To Array

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
-	$title = 'فروشگاه شارژ';
-	$description = 'شارژ آسان تلفن همراه';
-	$keywords = 'شارژ آسان تلفن همراه,شارژ موبایل, فروش شارژ,شارژ ایرانسل,شارژ همراه اول, رایتل,تالیا,کارت شارژ,شارژ مستقیم';
-	$webserviceID = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
-	$defaultOperator = 'MTN';
-	$defaultChargeKind = 'PIN';
+$config = array(
+	'title' => 'فروشگاه شارژ',
+	'description' => 'شارژ آسان تلفن همراه',
+	'keywords' => 'شارژ آسان تلفن همراه,شارژ موبایل, فروش شارژ,شارژ ایرانسل,شارژ همراه اول, رایتل,تالیا,کارت شارژ,شارژ مستقیم';
+	'webserviceID' => 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
+	'defaultOperator' => 'MTN',
+	'defaultChargeKind' => 'PIN'
+);
