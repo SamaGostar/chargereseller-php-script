711cffaaab43447582ea12de326960d5df4cb191
Alireza Rastgoo <a.rastgoo@gmail.com>
2014-06-26 10:35:02 +0430

    Update links and scripts

--- verify.php
+++ verify.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
 	<meta name="description" content="<?php echo $config['description']; ?>" />
 	<meta name="keywords" content="<?php echo $config['keywords']; ?>" />
-	<link href="/css/favicon.ico" type="image/x-icon" rel="icon" />
-	<link rel="stylesheet" type="text/css" href="/css/jquery.qtip.css" />
-	<link rel="stylesheet" type="text/css" href="/css/style.css" />
-	<link rel="stylesheet" type="text/css" href="/css/help.css" />
+	<link href="css/favicon.ico" type="image/x-icon" rel="icon" />
+	<link rel="stylesheet" type="text/css" href="css/jquery.qtip.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/help.css" />
 	<title><?php echo $config['title']; ?></title>
 </head>
 <body>
@@ -156,9 +156,9 @@
 			<div class="clear"></div>
 		</div>
 	</div>
-	<script type="text/javascript" src="/js/jquery-2.1.0.min.js"></script>
-	<script type="text/javascript" src="/js/jquery.qtip.min.js"></script>
-	<script type="text/javascript" src="/js/charge.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery.qtip.min.js"></script>
+	<script type="text/javascript" src="js/charge.js"></script>
 	<script type="text/javascript">
 		var WebserviceID = <?php echo '"' . $config['webserviceID'] . '"'; ?>;
 		var DefaultOperator = <?php echo '"' . $config['defaultOperator'] . '"'; ?>;
